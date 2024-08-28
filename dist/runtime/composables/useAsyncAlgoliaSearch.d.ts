9f8214b814b2617058474dbd3e9d5ffa3d459a69
Soultan Muhammad <soultanmuh@gmail.com>
2024-08-28 21:39:22 +0700

    fix

--- dist/runtime/composables/useAsyncAlgoliaSearch.d.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestOptionsObject, SearchResponse } from '../../types';
-import type { AsyncData } from '#app';
-export type AsyncSearchParams = {
-    query: string;
-    indexName?: string;
-    key?: string;
-} & RequestOptionsObject;
-export declare function useAsyncAlgoliaSearch({ query, requestOptions, indexName, key }: AsyncSearchParams): Promise<AsyncData<SearchResponse<unknown>, Error>>;
